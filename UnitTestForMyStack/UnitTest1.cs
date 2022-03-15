6d773cfce4dc77c81273e3acf8a5995e28bc986c
Дмитрий Урывчиков <mitya.mur@yandex.ru>
2022-03-15 19:42:11 +0300

    Tests are ready

--- UnitTestForMyStack/UnitTest1.cs
+++ UnitTestForMyStack/UnitTest1.cs
@@ -12,7 +12,77 @@ namespace UnitTestForMyStack
         {
             var st = new MyStack<int>();
 
-            Assert.AreEqual(st.Count, 0);
+            Assert.AreEqual(0, st.Count);
+        }
+
+        [TestMethod]
+        public void CountMustStayWithPeek()
+        {
+            var st = new MyStack<int>();
+            var cnt = 10;
+
+            for (int i = 0; i < cnt; i++) {
+                st.Push(i);
+            }
+
+            st.Peek();
+            Assert.AreEqual(cnt, st.Count);
+        }
+
+
+        [TestMethod]
+        public void CountMustIncreaseWithPush()
+        {
+            var st = new MyStack<int>();
+            var cnt = 10;
+
+            for (int i = 0; i < cnt; i++) {
+                st.Push(i);
+                Assert.AreEqual(i + 1, st.Count);
+            }
+        }
+
+        [TestMethod]
+        public void CountMustDecreaseWithPop()
+        {
+            var st = new MyStack<int>();
+            var cnt = 10;
+            
+            for (int i = 0; i < cnt; i++) {
+                st.Push(i);
+            }
+            for (int i = cnt-1; i >= 0; i--) {
+                st.Pop();
+                Assert.AreEqual(i, st.Count);
+            }
+        }
+        
+        [TestMethod]
+        public void ElemMustBeInStackAfterAdding()
+        {
+            var st = new MyStack<int>();
+            var cnt = 10;
+
+            for (int i = 0; i < cnt; i++) {
+                st.Push(i);
+                var p = st.Peek();
+                Assert.AreEqual(i, st.Peek());
+            }
+        }
+
+        [TestMethod]
+        public void AllElemsMustBeOnTheirPlaces()
+        {
+            var st = new MyStack<int>();
+            var cnt = 10;
+
+            for (int i = 0; i < cnt; i++) {
+                st.Push(i);
+            }
+
+            for (int i = cnt - 1; i >= 0; i--) {
+                Assert.AreEqual(i, st.Pop());
+            }
         }
     }
 }
