52031ee600559776349a5dbedc56166cfd2081f4
Дмитрий Урывчиков <mitya.mur@yandex.ru>
2022-03-15 18:26:04 +0300

    Added MyStack with fields, constructor methods

---
+++ Lab1/MyStack.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Linq;
+
+namespace Lab1
+{
+    public class MyStack<T>
+    {
+        #region Data
+        public int Count     { get; private set; }
+        private int _Capacity { get; set; }
+        private T[] _items   { get; set; }
+        #endregion
+
+        #region Constructors
+        public MyStack()
+        {
+            Count = 0;
+            _Capacity = 4;
+            _items = new T[_Capacity];
+        }
+        #endregion
+
+        #region Public methods
+        public void Push(T elem)
+        {
+            _items[Count++] = elem;
+            IncreaseCapacity();
+        }
+
+        public T Pop()
+        {
+            return _items[Count--];
+        }
+
+        public T Peek()
+        {
+            return _items[Count];
+        }
+
+        public bool Contains(T elem)
+        {
+            return _items.Contains(elem);
+        }
+        #endregion
+        
+        #region Private methods
+        private void IncreaseCapacity()
+        {
+            if (Count == _Capacity) {
+                _Capacity *= 2;
+                var newItems = new T[_Capacity];
+                Array.Copy(_items, newItems, Count);
+                _items = newItems;
+            }
+        }
+        #endregion
+    }
+}
