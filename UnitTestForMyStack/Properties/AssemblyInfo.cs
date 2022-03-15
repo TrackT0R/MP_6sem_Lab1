78f687c7d912004ad893c3b5b75883fc9022c351
Дмитрий Урывчиков <mitya.mur@yandex.ru>
2022-03-15 18:30:45 +0300

    Created unit test project

---
+++ UnitTestForMyStack/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestForMyStack")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestForMyStack")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("e1b833d3-3a85-4fb0-ac6a-618028c2695e")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTestForMyStack/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Lab1;
+
+namespace UnitTestForMyStack
+{
+    [TestClass]
+    public class UnitTest
+    {
+        [TestMethod]
+        public void CountMustBeZeroAfterCreation()
+        {
+            var st = new MyStack<int>();
+
+            Assert.AreEqual(st.Count, 0);
+        }
+    }
+}
